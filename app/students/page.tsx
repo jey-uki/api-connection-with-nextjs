5c94ab5422cba19d35297e2335d867911bb99623
jey <srjey3@gmail.com>
2026-06-12 10:29:54 +0530

    baseic structure of student list

---
+++ app/students/page.tsx
@@ -0,0 +1,10 @@
+import StudentListView from '@/sections/student/student-list-view'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <StudentListView />
+    </div>
+  )
+}
