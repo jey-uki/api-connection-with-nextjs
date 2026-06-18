d86ff949d55365ba24c18b45fcb50903cb8525aa
jeyanthiran <jeyanthiran97@gmail.com>
2026-06-18 23:21:40 +0530

    student form design modified

---
+++ app/students/new/page.tsx
@@ -0,0 +1,10 @@
+import StudentCreateView from '@/sections/student/view/student-create-view'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <StudentCreateView />
+    </div>
+  )
+}
--- sections/student/student-new-edit-form.tsx
+++ sections/student/student-new-edit-form.tsx
@@ -1,7 +1,213 @@
-import React from 'react'
+"use client"
+
+import * as React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { format } from "date-fns"
+
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Input } from "@/components/ui/input"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group"
+
+const formSchema = z.object({
+  fullName: z
+    .string("Full name is required.")
+    .min(2, "Full name must be at least 2 characters.")
+    .max(32, "Full name must be at most 32 characters."),
+  email: z
+    .string("Email is required.")
+    .email("Please enter a valid email address."),
+  age: z.string(),
+  joinDate: z.date(),
+})
 
 export default function StudentNewEditForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      age: undefined,
+      joinDate: undefined,
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast("You submitted the following values:", {
+      // sonner ExternalToast accepts 'description' for additional JSX content
+      description: (
+        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      position: "bottom-right",
+      classNames: {
+        content: "flex flex-col gap-2",
+      },
+      style: {
+        "--border-radius": "calc(var(--radius)  + 4px)",
+      } as React.CSSProperties,
+    })
+  }
+
   return (
-    <div>Student New Edit Form</div>
+    <Card className="w-full sm:max-w-md">
+      <CardHeader>
+        <CardTitle>Add New Student</CardTitle>
+        <CardDescription>
+          Enter the details of the new student.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              name="fullName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-full-name">
+                    Full Name
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-full-name"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter full name"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            {/* email */}
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter email"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            {/* age */}
+            <Controller
+              name="age"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Age</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type="number"
+                    value={field.value ?? ""}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter age"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            {/* join date */}
+            <Controller
+              name="joinDate"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field className="mx-auto">
+                  <FieldLabel htmlFor="date-picker-simple">
+                    Join Date
+                  </FieldLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        id="date-picker-simple"
+                        className="justify-start font-normal"
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        defaultMonth={field.value}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="button" variant="outline" onClick={() => form.reset()}>
+            Reset
+          </Button>
+          <Button type="submit" form="form-rhf-demo">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
   )
 }
